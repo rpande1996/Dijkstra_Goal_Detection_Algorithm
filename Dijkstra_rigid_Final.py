46447c4ed67617118292ab2f27b5ef2accea5d5e
rpande1996 <79412985+rpande1996@users.noreply.github.com>
2021-04-06 12:18:29 -0400

    Add files via upload
    Added clearance and radius values and created a new Queue class

--- Dijkstra_rigid_Final.py
+++ Dijkstra_rigid_Final.py
@@ -1,7 +1,30 @@
 import numpy as np
+import cv2
+
+radius = 10
+clearance = 5
+cl = radius + clearance
 
 obs_map = np.zeros((302, 402), dtype=int)
 obs_map[0, :] = 1
 obs_map[301, :] = 1
 obs_map[:, 0] = 1
 obs_map[:, 401] = 1
+
+class Queue:
+
+    # Creating a class to convert a list into a queue
+
+    def __init__(self):
+        self.queue = []
+
+    def add(self, node):
+        self.queue.append(node)
+
+    def pop(self):
+        ind = self.queue.index(min(self.queue))
+        node = self.queue.pop(ind)
+        return node
+
+    def __len__(self):
+        return len(self.queue)
