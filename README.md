5d7527c804b4def42dbc793dca18143ab7085443
Rajan Pande <panderajan1996@gmail.com>
2022-01-30 15:26:05 -0500

    Upload

--- README.md
+++ README.md
@@ -1,26 +1,85 @@
-Assignment done by:
-Rajan Pande: rpande@umd.edu
-Abhilash Mane: amane@umd.edu
+## AR_Plotting
+[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT)
+---
+## Contributors
 
-## How to run the code
+1) [Rajan Pande](https://github.com/rpande1996)
+Graduate Student of M.Eng Robotics at University of Maryland. 
+2) [Abhilash Mane](https://github.com/abhilash1998)
+Graduate Student of M.Eng Robotics at University of Maryland.
 
-Steps:
+## Overview
 
-1. Make sure you have Python 3 installed
-2. Make sure you have imported Numpy library
-3. Make sure you have imported OpenCV library
-4. Please input start points, end points, radius of robot and clearance
-5. The origin is at the bottom left, please mention points considering this.
-6. Please make sure that the points do not come in either border space or obstacle space
-7. The animated visualization is saved as a video
-8. The video will be saved in the same folder as the code
+This code utilizes for Dijkstra algorithm for path planning for a rigid robot of a certain radius and certain 
+clearance from the obstacles. The obstacles are padded with robot radius and clearance
 
-# Libraries installed
+## Softwares
 
-1. Numpy
-2. OpenCV
+* Recommended IDE: PyCharm 2021.2
 
-# Software Recommended
+## Libraries
 
-1. PyCharm as IDE
-2. Python 3.7
+* Numpy 1.21.2
+* OpenCV 3.4.8.29
+
+## Programming Languages
+
+* Python 3.8.12
+
+## License 
+
+```
+MIT License
+
+Copyright (c) 2021 Rajan Pande
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+SOFTWARE.
+```
+
+## Demo
+
+Following demo is for following parameters:
+```
+Robot radius: 5
+Clearance: 2
+Start Point: (8,8)
+Goal Point: (390, 290)
+```
+
+- [Dijkstra Algorithm Representation:](https://youtu.be/hZeOz0G5_iE)
+
+![ezgif com-gif-maker](https://github.com/rpande1996/Dijkstra_Goal_Detection_Algorithm/blob/main/media/gif/output.gif)
+
+## Build
+
+```
+git clone https://github.com/rpande1996/Dijkstra_Goal_Detection_Algorithm
+cd Dijkstra_Goal_Detection_Algorithm/src
+python Dijkstra_Goal_Detection.py
+```
+Enter the following parameters:-
+```
+Enter radius of the robot:
+Enter clearance:
+Enter start node x postion:
+Enter start node y postion:
+Enter goal node x postion:
+Enter goal node y postion:
+```
+Note: The start and goal nodes determine the position of the centre of the robot, consider the robot radius and clearance while selecting these coordinates
---
+++ src/Dijkstra_Goal_Detection.py
@@ -0,0 +1,387 @@
+import cv2
+import numpy as np
+
+try:
+    radius = int(input('Enter radius of the robot: '))
+    if radius < 0:
+        print("Invalid radius, setting radius to 0")
+        radius = 0
+
+    clearance = int(input('Enter clearance: '))
+    if clearance < 0:
+        print("Invalid clearance, setting clearance to 0")
+        clearance = 0
+
+except:
+    print("Error: Invalid Input. Exiting program")
+    exit(3)
+
+cl = radius + clearance
+
+# Map creation with edges as '1' in order to provide a void border of the map
+
+obs_map = np.zeros((302, 402), dtype=int)
+obs_map[0, :] = 1
+obs_map[301, :] = 1
+obs_map[:, 0] = 1
+obs_map[:, 401] = 1
+
+
+class Queue:
+
+    # Creating a class to convert a list into a queue
+
+    def __init__(self):
+        self.queue = []
+
+    def add(self, node):
+        self.queue.append(node)
+
+    def pop(self):
+        ind = self.queue.index(min(self.queue))
+        node = self.queue.pop(ind)
+        return node
+
+    def __len__(self):
+        return len(self.queue)
+
+
+# Creating a class to determine the node of the iteration. Node is the puzzle state.
+
+class Node:
+
+    # Defining the __init__ function
+
+    def __init__(self, data, parent, act, cost):
+        self.data = data
+        self.parent = parent
+        self.act = act
+        self.id = self.get_id()
+        self.cost = cost
+
+    def __eq__(self, other):
+        if hasattr(other, 'cost'):
+            return self.cost == other.cost
+        else:
+            raise NotImplementedError('Not supported between given types')
+
+    def __ne__(self, other):
+        if hasattr(other, 'cost'):
+            return self.cost != other.cost
+        else:
+            raise NotImplementedError('Not supported between given types')
+
+    def __lt__(self, other):
+        if hasattr(other, 'cost'):
+            return self.cost < other.cost
+        else:
+            raise NotImplementedError('Not supported between given types')
+
+    def __gt__(self, other):
+        if hasattr(other, 'cost'):
+            return self.cost > other.cost
+        else:
+            raise NotImplementedError('Not supported between given types')
+
+    def __le__(self, other):
+        if hasattr(other, 'cost'):
+            return self.cost <= other.cost
+        else:
+            raise NotImplementedError('Not supported between given types')
+
+    def __ge__(self, other):
+        if hasattr(other, 'cost'):
+            return self.cost >= other.cost
+        else:
+            raise NotImplementedError('Not supported between given types')
+
+    # Defining a function to generate a unique id of the state of the puzzle.
+
+    def get_id(self):
+        _id = np.ravel(self.data).tolist()
+        _id = [str(item) for item in _id]
+        _id = "-".join(_id)
+        self.id = _id
+        return self.id
+
+    # Defining the __repr__ function
+
+    def __repr__(self):
+        return str(self.data)
+
+
+# Creating a function to define the circle obstacle's area on the map
+
+def getCircleObstacle(i, j):
+    global cl
+    cond = ((j - 90) ** 2) + ((i - 70) ** 2) <= ((35 + cl) ** 2)
+    return cond
+
+
+# Creating a function to define the C shape obstacle's area on the map
+
+def getCShapeObstacle(i, j):
+    global cl
+    cond1 = i <= 270 - cl
+    cond2 = i <= 280 + cl
+    cond3 = j >= 200 - cl
+    cond4 = j >= 210 + cl
+    cond5 = i >= 240 + cl
+    cond6 = i >= 230 - cl
+    cond7 = j <= 230 + cl
+    ret_val = ((cond2 and cond3 and cond6 and cond7) and not (cond1 and cond4 and cond5 and cond7))
+    return ret_val
+
+
+# Creating a function to define the slanted rectangle obstacle's area on the map
+
+def getSlantedRectObstacle(i, j):
+    global cl
+    s1 = 0.7
+    s2 = -1.42814
+    x1 = np.arctan(s1)
+    x2 = np.arctan(s2)
+    d1 = np.cos(np.pi - x1)
+    d2 = np.cos(np.pi - x2)
+    a = -(cl / d1)
+    b = -(cl / d2)
+    cond1 = (i) + (1.42814 * j) >= (176.5511 - b)
+    cond2 = (i) - (0.7 * j) >= (74.39 - a)
+    cond3 = (i) + (1.42814 * j) <= (428.06815 + b)
+    cond4 = (i) - (0.7 * j) <= (98.80545 + a)
+    ret_val = (cond1 and cond2 and cond3 and cond4)
+    return ret_val
+
+
+# Creating a function to define the ellipse obstacle's area on the map
+
+def getEllipseObstacle(i, j):
+    global cl
+    cond = (((j - 246) / (60 + cl)) ** 2) + (((i - 145) / (30 + cl)) ** 2) <= 1
+    return cond
+
+
+def getBorderClearance(i, j):
+    global cl
+    cond1 = j >= 402 - cl
+    cond2 = j <= cl
+    cond3 = i >= 302 - cl
+    cond4 = i <= cl
+    ret_val = cond1 or cond2 or cond3 or cond4
+    return ret_val
+
+
+# Creating an if-condition to change value of the element in area under all obstacles to '1' in order to create a void in the map
+
+for i in range(obs_map.shape[0]):
+    for j in range(obs_map.shape[1]):
+        if getCircleObstacle(obs_map.shape[0] - i, j) or getCShapeObstacle(obs_map.shape[0] - i,
+                                                                           j) or getSlantedRectObstacle(
+            obs_map.shape[0] - i, j) or getEllipseObstacle(obs_map.shape[0] - i, j) or getBorderClearance(
+            obs_map.shape[0] - i, j):
+            obs_map[i, j] = 1
+
+
+# Defining the move functions
+# Defining the move up function where if the element above does not have '1' value, i.e. if there isn't a void in the element above, the object moves up
+
+def move_up(i, j):
+    if obs_map[i - 1, j] != 1:
+        return (i - 1, j)
+
+
+# Defining the move down function where if the element below does not have '1' value, i.e. if there isn't a void in the element below, the object moves down
+
+def move_down(i, j):
+    if obs_map[i + 1, j] != 1:
+        return (i + 1, j)
+
+
+# Defining the move left function where if the element on the left does not have '1' value, i.e. if there isn't a void in the element on the left, the object moves left
+
+def move_left(i, j):
+    if obs_map[i, j - 1] != 1:
+        return (i, j - 1)
+
+
+# Defining the move right function where if the element on the right does not have '1' value, i.e. if there isn't a void in the element on the right, the object moves right
+
+def move_right(i, j):
+    if obs_map[i, j + 1] != 1:
+        return (i, j + 1)
+
+
+# Defining the move up left function where if the element above and left does not have '1' value, i.e. if there isn't a void in the element above and left , the object moves up left
+
+def move_up_left(i, j):
+    if obs_map[i - 1, j - 1] != 1:
+        return (i - 1, j - 1)
+
+
+# Defining the move up right function where if the element above and right does not have '1' value, i.e. if there isn't a void in the element above and right, the object moves up right
+
+def move_up_right(i, j):
+    if obs_map[i - 1, j + 1] != 1:
+        return (i - 1, j + 1)
+
+
+# Defining the move down left function where if the element below and left does not have '1' value, i.e. if there isn't a void in the element below and left, the object moves down left
+
+def move_down_left(i, j):
+    if obs_map[i + 1, j - 1] != 1:
+        return (i + 1, j - 1)
+
+
+# Defining the move down right function where if the element below and right does not have '1' value, i.e. if there isn't a void in the element below and right, the object moves down right
+
+def move_down_right(i, j):
+    if obs_map[i + 1, j + 1] != 1:
+        return (i + 1, j + 1)
+
+
+# Defining a function to generate new legal moves as per the state
+
+def generate_new_moves(state):
+    list_states = []
+    for func in [move_left, move_right, move_down, move_up]:
+        cost = state.cost + 1
+        dum_state = state.data
+        out_state = func(dum_state[0], dum_state[1])
+        if out_state is not None:
+            list_states.append((out_state, cost))
+    for func in [move_up_left, move_up_right, move_down_left,
+                 move_down_right]:
+        cost = state.cost + 1.414
+        dum_state = state.data
+        out_state = func(dum_state[0], dum_state[1])
+        if out_state is not None:
+            list_states.append((out_state, cost))
+    return list_states
+
+
+# Inputting values from the user and checking if the values are valid by checking the outbound values and in-obstacle values
+
+try:
+    start_node_x = int(input('Enter start node x postion: '))
+    if start_node_x < 0:
+        print("Invalid start node x position, setting x postion to 0")
+        start_node_x = 0
+    elif start_node_x > 402:
+        print("Invalid start node x position, setting x postion to 403")
+        start_node_x = 402
+
+    start_node_y = int(input('Enter start node y postion: '))
+    if start_node_y < 0:
+        print("Invalid start node y position, setting y postion to 0")
+        start_node_y = 0
+    elif start_node_y > 302:
+        print("Invalid start node y position, setting y postion to 300")
+        start_node_y = 302
+
+    goal_node_x = int(input('Enter goal node x postion: '))
+    if goal_node_x < 0:
+        print("Invalid goal node x position, setting x postion to 0")
+        goal_node_x = 0
+    elif goal_node_x > 402:
+        print("Invalid goal node x position, setting x postion to 403")
+        start_node_x = 402
+
+    goal_node_y = int(input('Enter goal node y postion: '))
+    if goal_node_y < 0:
+        print("Invalid goal node y position, setting y postion to 0")
+        goal_node_y = 0
+    elif goal_node_y > 302:
+        print("Invalid goal node y position, setting y postion to 300")
+        start_node_y = 302
+
+    if obs_map[obs_map.shape[0] - start_node_y, start_node_x] == 1:
+        print("Error: Start position is in void space. Exiting program")
+        exit(1)
+
+    if obs_map[obs_map.shape[0] - goal_node_y, goal_node_x] == 1:
+        print("Error: Goal position is in void space. Exiting program")
+        exit(1)
+except:
+    print("Error: Invalid Input. Exiting program")
+    exit(2)
+
+# Creating the goal state and initial state.
+
+goal_state = (obs_map.shape[0] - goal_node_y, goal_node_x)
+init_state = (obs_map.shape[0] - start_node_y, start_node_x)
+state_queue = Queue()
+state_queue.add(Node(init_state, None, None, 0))
+
+visited = []
+
+# Creating a new array in order to write as video
+
+result_map = obs_map.copy()
+result_map = result_map * 255
+result_map = np.dstack((result_map, result_map, result_map))
+result_map = result_map.astype(np.uint8)
+height, width = obs_map.shape
+FPS_val = 240
+
+video_save = cv2.VideoWriter("../media/output/Path_detection.mp4", cv2.VideoWriter_fourcc(*'mp4v'), FPS_val, (width, height))
+
+# While loop to iterate the values inside the array with legal moves.
+# If the current state is same as the goal state then the loop breaks.
+# If the state ID is found in the visited list, then the node is skipped
+
+while True:
+    try:
+        cur_node = state_queue.pop()
+    except:
+        if len([node for node in state_queue.queue]) == 0:
+            break
+    if np.all(cur_node.data == goal_state):
+        break
+    if cur_node.id in visited:
+        continue
+    moves = generate_new_moves(cur_node)
+    for move in moves:
+        new_node = Node(move[0], cur_node, None, move[1])
+        state_queue.add(new_node)
+    visited.append(cur_node.id)
+
+    result_map[cur_node.data[0], cur_node.data[1], :] = np.asarray((255, 0, 0))
+
+    result_map[result_map.shape[0] - start_node_y, start_node_x, :] = np.asarray((0, 0, 255))
+    result_map[result_map.shape[0] - goal_node_y, goal_node_x, :] = np.asarray((0, 255, 0))
+
+    video_save.write(result_map)
+
+target_node = cur_node
+path = []
+
+# While loop to add a step in the path
+
+while cur_node is not None:
+    path.append(cur_node)
+    cur_node = cur_node.parent
+
+# Traceback the path
+
+path.reverse()
+
+# Converting the data in path array to BGR values
+
+for item in path:
+    result_map[item.data[0], item.data[1], :] = np.asarray((0, 255, 255))
+    result_map[result_map.shape[0] - start_node_y, start_node_x, :] = np.asarray((0, 0, 255))
+    result_map[result_map.shape[0] - goal_node_y, goal_node_x, :] = np.asarray((0, 255, 0))
+
+    for _ in range(int(FPS_val / 20)):
+        video_save.write(result_map)
+
+# Writing and saving the complete traverse
+
+video_save.write(result_map)
+
+video_save and video_save.release()
+cv2.imshow("Path", result_map)
+if cv2.waitKey(0) and 0XFF == ord('q'):
+    exit(0)
+
+cv2.destroyAllWindows()
